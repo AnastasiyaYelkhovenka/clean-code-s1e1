d5fdc07cffb98e89774dbb8886a0e014b8b813e8
Anastasiya Yelkhovenka <23nastya1627@gmail.com>
2025-10-22 00:29:06 +0100

    refactor: delete unnecessary comments, indent (deleteTask, taskCompleted, taskIncomplete, bindTaskEvents functions)

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-
-// Event handling, user interaction is what starts the code execution.
-
 const taskInput = document.getElementById("new-task-input");
 const addButton = document.querySelector("#new-task-form .new-task__submit");
-const form = document.getElementById("new-task-form");
 const incompleteTaskHolder = document.getElementById("incomplete-tasks");
 const completedTasksHolder = document.getElementById("completed-tasks");
 
 
 const createNewTaskElement = function (taskString) {
-
   const listItem = document.createElement("li");
+  listItem.className = "todo__item";
 
   const checkBox = document.createElement("input");
   checkBox.type = "checkbox";
@@ -90,87 +79,47 @@ const editTask = function () {
 };
 
 
-//Delete task.
 const  deleteTask = function () {
-    const listItem = this.parentNode;
-    const ul = listItem.parentNode;
-    //Remove the parent list item from the ul.
-    ul.removeChild(listItem);
-
+  const listItem = this.parentNode;
+  const ul = listItem.parentNode;
+  ul.removeChild(listItem);
 }
 
 
-//Mark task completed
 const taskCompleted = function () {
-    //Append the task list item to the #completed-tasks
-    const listItem = this.parentNode;
-    completedTasksHolder.appendChild(listItem);
-    bindTaskEvents(listItem, taskIncomplete);
-
+  const listItem = this.parentNode;
+  completedTasksHolder.appendChild(listItem);
+  bindTaskEvents(listItem, taskIncomplete);
 }
 
 
 const taskIncomplete = function () {
-//Mark task as incomplete.
-    //When the checkbox is unchecked
-    //Append the task list item to the #incompleteTasks.
-    const listItem = this.parentNode;
-    incompleteTaskHolder.appendChild(listItem);
-    bindTaskEvents(listItem,taskCompleted);
+  const listItem = this.parentNode;
+  incompleteTaskHolder.appendChild(listItem);
+  bindTaskEvents(listItem,taskCompleted);
 }
 
 
-
-// const ajaxRequest = function () {
-    // console.log("AJAX Request");
-// }
-
-//The glue to hold it all together.
-
-
-//Set the click handler to the addTask function.
 addButton.onclick=addTask;
 addButton.addEventListener("click",addTask);
-// addButton.addEventListener("click",ajaxRequest);
 
 
 const bindTaskEvents = function (taskListItem,checkBoxEventHandler) {
-//select ListItems children
-    const checkBox = taskListItem.querySelector(".todo__checkbox");
-    const editButton = taskListItem.querySelector(".todo__btn--edit, .todo__btn--save");
-    const deleteButton = taskListItem.querySelector(".todo__btn--delete");
+  const checkBox = taskListItem.querySelector(".todo__checkbox");
+  const editButton = taskListItem.querySelector(".todo__btn--edit, .todo__btn--save");
+  const deleteButton = taskListItem.querySelector(".todo__btn--delete");
 
-
-    //Bind editTask to edit button.
-    editButton.onclick=editTask;
-    //Bind deleteTask to delete button.
-    deleteButton.onclick=deleteTask;
-    //Bind taskCompleted to checkBoxEventHandler.
-    checkBox.onchange=checkBoxEventHandler;
+  editButton.onclick=editTask;
+  deleteButton.onclick=deleteTask;
+  checkBox.onchange=checkBoxEventHandler;
 }
 
-//cycle over incompleteTaskHolder ul list items
-//for each list item
+
 for (const i = 0; i < incompleteTaskHolder.children.length; i++){
-
-    //bind events to list items chldren(tasksCompleted)
-    bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
+  bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
 }
 
 
-
-
-//cycle over completedTasksHolder ul list items
 for (const i = 0; i < completedTasksHolder.children.length; i++){
-    //bind events to list items chldren(tasksIncompleted)
-    bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
+  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
-
-
-
-
-// Issues with usability don't get seen until they are in front of a human tester.
-
-//prevent creation of empty tasks.
-
-//Change edit to save when you are in edit mode.

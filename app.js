3cbdebbcf708c535ab82f7c000b92c1474a77145
Anastasiya Yelkhovenka <23nastya1627@gmail.com>
2025-10-21 23:59:07 +0100

    refactor: update function bindTaskEvents with const

--- app.js
+++ app.js
@@ -158,12 +158,12 @@ addButton.addEventListener("click",addTask);
 addButton.addEventListener("click",ajaxRequest);
 
 
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
+const bindTaskEvents = function (taskListItem,checkBoxEventHandler) {
     console.log("bind list item events");
 //select ListItems children
-    var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    const checkBox = taskListItem.querySelector(".todo__checkbox");
+    const editButton = taskListItem.querySelector(".todo__btn--edit, .todo__btn--save");
+    const deleteButton = taskListItem.querySelector(".todo__btn--delete");
 
 
     //Bind editTask to edit button.
